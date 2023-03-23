1fd2d0c1315ceb6ad77cf408bdd34df2e3d17055
SamiEroglu <samie.github@gmail.com>
2023-03-23 17:31:38 +0300

    fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
 import Newheader from "./component/Newheader";
 import Newikincihead from "./component/Newikincihead";
@@ -12,210 +12,210 @@ import Newft from "./component/Newft";
 import Avatar from "./component/Avatar";
 import ScrollButton from "./component/ScrollButton";
 import {
-	Mavibeyazkutu,
-	Cnt0,
-	Cnt1,
-	Cnt2,
-	Cnt3,
-	Berkavatar,
-	Ilgazavatar,
-	Services,
-	Iconbaglanti,
-	Iconetiket,
-	Kaydetico,
+  Mavibeyazkutu,
+  Cnt0,
+  Cnt1,
+  Cnt2,
+  Cnt3,
+  Berkavatar,
+  Ilgazavatar,
+  Services,
+  Iconbaglanti,
+  Iconetiket,
+  Kaydetico,
 } from "./icon";
 import cpuImage from "./img/cpu.png";
 
 function App() {
-	return (
-		<div className="App">
-			<Newheader />
-			<Newikincihead />
-			<Wordsmetablocknote />
-			<NewAtelier />
-			<Futures />
-			<Degenlabs />
-			<Nowseedlarva />
-			<div
-				style={{ backgroundImage: `url(${cpuImage})` }}
-				className="collapsible-container"
-			>
-				<Services />
-				<Collapsible
-					icon={<Mavibeyazkutu />}
-					title="n3C4 WEB 3.0 COMMUNITY-LIBRARY"
-					btn={<button className="button-bir">FREE FOR ALL</button>}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt2 />}
-					title="create a unique cryptocurrency"
-					btn={<div className="fiveweeks">+5 WEEKS / PAID</div>}
-					ico={<Iconbaglanti />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt2 />}
-					title="Creating and marketing on NFTs"
-					btn={<button className="button-iki">COLAB OPEN</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt2 />}
-					title="Marketplaces for Non-fungible Tokens"
-					btn={<button className="button-iki">COLAB OPEN</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt1 />}
-					title="DAO Research and Dev."
-					btn={<button className="button-bir">FREE FOR ALL</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt0 />}
-					title="Web3 Gamification Projects"
-					btn={<button className="button-iki">COLAB OPEN</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt0 />}
-					title="Web3 apps for brands: DApps"
-					btn={<div className="fiveweeks"> +7 WEEKS / PAID</div>}
-					ico={<Kaydetico />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible icon={<Cnt2 />} title="UI and backend web designs">
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt3 />}
-					title="Web 3.0 Community Making Services (TW, IG, DC)"
-					btn={<button className="button-iki">COLAB OPEN</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible icon={<Cnt0 />} title="Art direction and branding service">
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt0 />}
-					title="BRAND BUILDING for gaming/progaming brandS"
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt3 />}
-					title="Sustainable Web 3.0"
-					btn={<button className="button-iki">COLAB OPEN</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt2 />}
-					title="Community build for community use"
-					btn={<button className="button-bir">FREE FOR ALL</button>}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-				<Collapsible
-					icon={<Cnt1 />}
-					title="COMMUNITY DAPP: WHITEPAPER.IO"
-					btn={<button className="button-bir">FREE FOR ALL</button>}
-					ico={<Iconetiket />}
-				>
-					<p>
-						I am going to assume that you know the basics like how a component
-						works, how useState works, and how to set up React. I will only
-						focus on building the project and though this is for an absolute
-						beginner who would like to know how useEffect and useRef work.
-					</p>
-				</Collapsible>
-			</div>
-			<Avatar icon={<Berkavatar />} icon1={<Ilgazavatar></Ilgazavatar>} />
-			<Newft />
+  return (
+    <div className="App">
+      <Newheader />
+      <Newikincihead />
+      <Wordsmetablocknote />
+      <NewAtelier />
+      <Futures />
+      <Degenlabs />
+      <Nowseedlarva />
+      <div
+        style={{ backgroundImage: `url(${cpuImage})` }}
+        className="collapsible-container"
+      >
+        <Services />
+        <Collapsible
+          icon={<Mavibeyazkutu />}
+          title="n3C4 WEB 3.0 COMMUNITY-LIBRARY"
+          btn={<button className="button-bir">FREE FOR ALL</button>}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt2 />}
+          title="create a unique cryptocurrency"
+          btn={<div className="fiveweeks">+5 WEEKS / PAID</div>}
+          ico={<Iconbaglanti />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt2 />}
+          title="Creating and marketing on NFTs"
+          btn={<button className="button-iki">COLAB OPEN</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt2 />}
+          title="Marketplaces for Non-fungible Tokens"
+          btn={<button className="button-iki">COLAB OPEN</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt1 />}
+          title="DAO Research and Dev."
+          btn={<button className="button-bir">FREE FOR ALL</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt0 />}
+          title="Web3 Gamification Projects"
+          btn={<button className="button-iki">COLAB OPEN</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt0 />}
+          title="Web3 apps for brands: DApps"
+          btn={<div className="fiveweeks"> +7 WEEKS / PAID</div>}
+          ico={<Kaydetico />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible icon={<Cnt2 />} title="UI and backend web designs">
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt3 />}
+          title="Web 3.0 Community Making Services (TW, IG, DC)"
+          btn={<button className="button-iki">COLAB OPEN</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible icon={<Cnt0 />} title="Art direction and branding service">
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt0 />}
+          title="BRAND BUILDING for gaming/progaming brandS"
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt3 />}
+          title="Sustainable Web 3.0"
+          btn={<button className="button-iki">COLAB OPEN</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt2 />}
+          title="Community build for community use"
+          btn={<button className="button-bir">FREE FOR ALL</button>}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+        <Collapsible
+          icon={<Cnt1 />}
+          title="COMMUNITY DAPP: WHITEPAPER.IO"
+          btn={<button className="button-bir">FREE FOR ALL</button>}
+          ico={<Iconetiket />}
+        >
+          <p>
+            I am going to assume that you know the basics like how a component
+            works, how useState works, and how to set up React. I will only
+            focus on building the project and though this is for an absolute
+            beginner who would like to know how useEffect and useRef work.
+          </p>
+        </Collapsible>
+      </div>
+      <Avatar icon={<Berkavatar />} icon1={<Ilgazavatar></Ilgazavatar>} />
+      <Newft />
 
-			<ScrollButton />
-		</div>
-	);
+      <ScrollButton />
+    </div>
+  );
 }
 
 export default App;
